761158e432fc66cfc2ab0f0ae14d689d1dc7c129
izebhigie <dizebhigie@gmail.com>
2024-07-03 16:40:42 +0100

    update

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ document.addEventListener('DOMContentLoaded', () => {
     updateTime();
     setInterval(updateTime, 1000);
 });
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
